466b78464f8d2a2de2273052deac6f0d16798da2
mickemersmak <mickemersmak@gmail.com>
2025-11-24 18:00:22 +0100

    Switch to Ethereum Sepolia testnet

--- lib/wagmi.ts
+++ lib/wagmi.ts
@@ -1,9 +1,9 @@
 import { http, createConfig } from 'wagmi'
-import { mainnet, sepolia } from 'wagmi/chains'
+import { sepolia } from 'wagmi/chains'
 import { coinbaseWallet, walletConnect } from 'wagmi/connectors'
 
 export const config = createConfig({
-  chains: [mainnet, sepolia],
+  chains: [sepolia],
   connectors: [
     walletConnect({
       projectId: process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID || 'demo',
@@ -11,7 +11,6 @@ export const config = createConfig({
     coinbaseWallet({ appName: 'Caspers Burger NFT' }),
   ],
   transports: {
-    [mainnet.id]: http(),
     [sepolia.id]: http(),
   },
 })
